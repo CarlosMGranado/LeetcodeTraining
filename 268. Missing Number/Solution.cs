f5192933ecb88261572feb59cabb7ac2a5816409
CarlosMGranado <141454611+CarlosMGranado@users.noreply.github.com>
2026-02-02 20:33:08 +0000

    02/02/2026

---
+++ 268. Missing Number/Solution.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MissingNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i <= nums.Length; i++) {
+            result ^= i;
+        }
+
+        for (int i = 0; i < nums.Length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
